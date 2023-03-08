73c68200db0a5954ed7c3d35e5022640735fd845
KazumasaYasui <silver.soul39@gmail.com>
2023-03-08 23:42:18 +0900

    Cloud Resource Manager APIの有効化＆該当メンバーにストレージ管理者ロールを付与

--- main.tf
+++ main.tf
@@ -9,3 +9,26 @@ resource "google_storage_bucket" "sample-bucket" {
   location = "ASIA-NORTHEAST1"
   name     = "kazumasa-yasui-sample-bucket"
 }
+
+locals {
+  services = toset([
+    "cloudresourcemanager.googleapis.com",
+  ])
+
+  roles = toset([
+    "roles/storage.admin",
+  ])
+}
+
+resource "google_project_service" "service" {
+  for_each = local.services
+  project  = var.project
+  service  = each.value
+}
+
+resource "google_project_iam_member" "sample-project-role" {
+  for_each = local.roles
+  project  = var.project
+  role     = each.value
+  member   = "user:kazumasa.yasui39@gmail.com"
+}
